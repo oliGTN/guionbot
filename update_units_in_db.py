cdadf65626744ae198504e2c1b91020f4e18798c
Olivier Guitton <olivier.w.guitton@gmail.com>
2021-03-06 00:50:58 +0100

    update character list

--- update_units_in_db.py
+++ update_units_in_db.py
@@ -6,9 +6,9 @@ import connect_mysql
 unitsList = json.load(open('..\DATA'+os.path.sep+'unitsList.json', 'r'))
 
 for unit in unitsList:
-    if (not ':' in unit['id']):
-        connect_mysql.update_unit(unit)
-
-    # if unit['id'] == "AURRA_SING":
-        # print(unit['id'])
+    # if (not ':' in unit['id']):
         # connect_mysql.update_unit(unit)
+
+    if unit['id'] in ['ARMORER', 'THEMANDALORIANBESKARARMOR', 'DARKTROOPER']:
+        print(unit['id'])
+        connect_mysql.update_unit(unit)
