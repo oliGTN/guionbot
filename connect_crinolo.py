bf67f90c7f97a5d0c5f15674fac20773c29e424e
Olivier Guitton <olivier.w.guitton@gmail.com>
2021-04-08 23:21:41 +0200

    add robustness in case combatType = None

--- connect_crinolo.py
+++ connect_crinolo.py
@@ -4,9 +4,24 @@ import json
 crinolo_url = 'https://swgoh-stat-calc.glitch.me/api?flags=statIDs,unscaled'
 
 def add_stats(dict_player):
+    # Add robustness in case combatType not defined
+    # These characters are removed from the stat request
+    list_buggy_characters=[]
+    for character in dict_player['roster']:
+        if character['combatType'] == None:
+            print("WAR: unknown combatType for "+character['defId']+" > assume =1")
+            character['combatType'] = 1
+            list_buggy_characters.append(character)
+            dict_player['roster'].remove(character)
+
     r=requests.post(crinolo_url, json=[dict_player])
     if r.status_code != 200:
         print('ERR: Cannot connect to crinolo API')
-        return  dict_player
+        print(r)
+        return dict_player
     
+    # Re-put the buggy characters before going to update player
+    for character in list_buggy_characters:
+        dict_player['roster'].append(character)
+        
     return json.loads(r.content.decode('utf-8'))[0]
