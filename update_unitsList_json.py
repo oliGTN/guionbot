eb0afce2ec776b45f8c34cf5da9ab0843b1d75e7
oliGTN <olivier.w.guitton@gmail.com>
2021-04-29 21:57:43 +0200

    official methodology to get playable characters

--- update_unitsList_json.py
+++ update_unitsList_json.py
@@ -9,11 +9,12 @@ import os
 unitsList = json.load(open('DATA'+os.path.sep+'unitsList.json', 'r'))
 
 unitsList_dict = {}
-for unit in unitsList:
-    if (not ':' in unit['id']):
-        if unit['id'] in unitsList_dict:
-            print('WAR: double definition of '+unit['id'])
-        unitsList_dict[unit['id']] = unit
+unitsList_obtainable = [x for x in list(filter(lambda f:f['rarity']==7 and f['obtainable'] and f['obtainableTime']==0, unitsList))]
+for unit in unitsList_obtainable:
+    #if (not ':' in unit['id']):
+        if unit['baseId'] in unitsList_dict:
+            print('WAR: double definition of '+unit['baseId'])
+        unitsList_dict[unit['baseId']] = unit
 
 fnew = open('DATA'+os.path.sep+'unitsList_dict.json', 'w')
 fnew.write(json.dumps(unitsList_dict, sort_keys=True, indent=4))
@@ -22,37 +23,40 @@ fnew.close()
 ############################################
 # It also creates a dictionary of aliases from nameKeys
 ############################################
-unitsList_FRE_FR = unitsList
+unitsList_FRE_FR_obtainable = unitsList_obtainable
 unitsList_ENG_US = json.load(open('DATA'+os.path.sep+'unitsList_ENG_US.json', 'r'))
-priority_names=["LEGENDARY", "S3", "GLREY", "HOTHREBELSOLDIER", "IMPERIALPROBEDROID", "AURRA_SING", "AMILYNHOLDO", "VULTUREDROID", "GRIEVOUS", "B1BATTLEDROIDV2", "THEMANDALORIAN", "VADER", "OBJ_CRATE_01", "SCOOTTROOPER"]
+unitsList_ENG_US_obtainable = [x for x in list(filter(lambda f:f['rarity']==7 and f['obtainable'] and f['obtainableTime']==0, unitsList_ENG_US))]
+#priority_names=["LEGENDARY", "S3", "GLREY", "HOTHREBELSOLDIER", "IMPERIALPROBEDROID", "AURRA_SING", "AMILYNHOLDO", "VULTUREDROID", "GRIEVOUS", "B1BATTLEDROIDV2", "THEMANDALORIAN", "VADER", "OBJ_CRATE_01", "SCOOTTROOPER"]
+priority_names=[]
+
 unitsAlias_dict = {}
-for unitsList in [unitsList_FRE_FR, unitsList_ENG_US]:
+for unitsList in [unitsList_FRE_FR_obtainable, unitsList_ENG_US_obtainable]:
     for unit in unitsList:
-        if (not ':' in unit['id']) and not "EVENT" in unit['id'] \
-                                   and not "PVE" in unit['id'] \
-                                   and not "DUEL" in unit['id']:
+        #if (not ':' in unit['id']) and not "EVENT" in unit['id'] \
+        #                           and not "PVE" in unit['id'] \
+        #                           and not "DUEL" in unit['id']:
             #manage characters with number and nickname (eg: clones)
             names = unit['nameKey'].split('"')
             if "" in names:
                 names.remove("")
             for nameKey in names:
                 if nameKey in unitsAlias_dict:
-                    if unitsAlias_dict[nameKey][1] != unit['id']:
+                    if unitsAlias_dict[nameKey][1] != unit['baseId']:
                         prio_found = False
                         for prio in priority_names:
-                            if prio in unit['id']:
-                                unitsAlias_dict[nameKey] = [unitsList_dict[unit['id']]['nameKey'], unit['id']]
+                            if prio in unit['baseId']:
+                                unitsAlias_dict[nameKey] = [unitsList_dict[unit['baseId']]['nameKey'], unit['baseId']]
                                 prio_found = True
                             elif prio in unitsAlias_dict[nameKey][1]:
                                 prio_found = True
                         if not prio_found:
                             print('WAR: double definition of '+nameKey)
                             print(unitsAlias_dict[nameKey][1])
-                            print(unit['id'])
+                            print(unit['baseId'])
                     else:
                         pass
                 else:
-                    unitsAlias_dict[nameKey] = [unitsList_dict[unit['id']]['nameKey'], unit['id']]
+                    unitsAlias_dict[nameKey] = [unitsList_dict[unit['baseId']]['nameKey'], unit['baseId']]
 
 fnew = open('DATA'+os.path.sep+'unitsAlias_dict.json', 'w')
 fnew.write(json.dumps(unitsAlias_dict, sort_keys=True, indent=4))
