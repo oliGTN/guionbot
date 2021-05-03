8e8a18483c57d1e5ba944ee29c48b8cbe5ad3365
oliGTN <olivier.w.guitton@gmail.com>
2021-05-03 21:18:33 +0200

    display new caharcter when updating

--- update_crinolo_data.py
+++ update_crinolo_data.py
@@ -14,7 +14,13 @@ fcrinolo.close()
 fcrinolo = open('DATA'+os.path.sep+'crinolo_gameData.json', 'r')
 crinolo_gameData = json.load(fcrinolo)
 
+prev_list_characters_crinolo = json.load(open('DATA'+os.path.sep+'crinolo_gameData_light.json', 'r'))
 list_characters_crinolo = list(crinolo_gameData['unitData'].keys())
 fcrinolo_light = open('DATA'+os.path.sep+'crinolo_gameData_light.json', 'w')
 fcrinolo_light.write(json.dumps(list_characters_crinolo))
 fcrinolo_light.close()
+
+# Display differences
+for char in list_characters_crinolo:
+    if not char in prev_list_characters_crinolo:
+        print("NEW: "+char)
