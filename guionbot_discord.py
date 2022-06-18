56eb477cbcbf30b49589d8bd08abcfecee8790e1
oliGTN <olivier.w.guitton@gmail.com>
2022-06-18 21:28:19 +0200

    fix error status when manage_me does not recognize a discord id

--- guionbot_discord.py
+++ guionbot_discord.py
@@ -2495,7 +2495,7 @@ class MemberCog(commands.Cog, name="Commandes pour les membres"):
             
         allyCode= manage_me(ctx, allyCode)
         if allyCode[0:3] == 'ERR':
-            await ctx.send(allyCode_attack)
+            await ctx.send(allyCode)
             await ctx.message.add_reaction(emoji_error)
             return
 
@@ -2632,7 +2632,7 @@ class MemberCog(commands.Cog, name="Commandes pour les membres"):
 
         allyCode= manage_me(ctx, allyCode)
         if allyCode[0:3] == 'ERR':
-            await ctx.send(allyCode_attack)
+            await ctx.send(allyCode)
             await ctx.message.add_reaction(emoji_error)
             return
 
