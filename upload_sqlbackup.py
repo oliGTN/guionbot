96d3dc5f72eb360fba4d44025e91b6a95d7146da
oliGTN <olivier.w.guitton@gmail.com>
2022-10-10 01:07:28 +0100

    replace instead of create

--- upload_sqlbackup.py
+++ upload_sqlbackup.py
@@ -3,6 +3,7 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 from oauth2client.service_account import ServiceAccountCredentials
 import json
+import sys
 
 gauth = GoogleAuth()
 scope = ["https://www.googleapis.com/auth/drive"]
@@ -11,9 +12,15 @@ creds_json = json.loads(creds_envVar)
 
 gauth.credentials = ServiceAccountCredentials.from_json_keyfile_dict(creds_json, scope)
 drive = GoogleDrive(gauth)
-fid=config.GDRIVE_SQLBACKUP_ID
+dir_id=config.GDRIVE_SQLBACKUP_ID
 fpath=config.SQLBACKUP_GZ
-f = drive.CreateFile({"title": "all_tables.gz", "parents": [{"kind": "drive#fileLink", "id": fid}]})
+if len(sys.argv)>1 and sys.argv[1] == "create":
+    f = drive.CreateFile({"title": "all_tables.gz", "parents": [{"kind": "drive#fileLink", "id": dir_id}]})
+else:
+    file_list = drive.ListFile({'q': "'"+str(dir_id)+"' in parents and trashed=false"}).GetList()
+    fid = (file_list[0]['id'])
+    f = drive.CreateFile({"id": fid})
+
 f.SetContentFile(fpath)
 f.Upload()
 
