9f0f66cbae7626aed837abf3a0b6bf9db68765ff
oliGTN <olivier.w.guitton@gmail.com>
2023-02-03 20:58:33 +0100

    adapt to new path and to several file in gdrive

--- SCRIPTS/upload_sqlbackup.py
+++ SCRIPTS/upload_sqlbackup.py
@@ -18,7 +18,8 @@ if len(sys.argv)>1 and sys.argv[1] == "create":
     f = drive.CreateFile({"title": "all_tables.gz", "parents": [{"kind": "drive#fileLink", "id": dir_id}]})
 else:
     file_list = drive.ListFile({'q': "'"+str(dir_id)+"' in parents and trashed=false"}).GetList()
-    fid = (file_list[0]['id'])
+    filtered_file_list = [x for x in file_list if x['title']=="all_tables.gz"]
+    fid = (filtered_file_list[0]['id'])
     f = drive.CreateFile({"id": fid})
 
 f.SetContentFile(fpath)
