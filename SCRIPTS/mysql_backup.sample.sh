a4a16d7212838e3f3bca9925249707340230cb23
oliGTN <olivier.w.guitton@gmail.com>
2023-02-12 13:48:36 +0100

    update paths and add EVENTS

--- SCRIPTS/mysql_backup.sample.sh
+++ SCRIPTS/mysql_backup.sample.sh
@@ -1,10 +1,13 @@
-rm /home/pi/GuionBot/SQLBACKUP/*
+rm -rf /home/pi/GuionBot/SQLBACKUP/
+mkdir /home/pi/GuionBot/SQLBACKUP
 for table in gp_history gv_history roster_evolutions guilds shards stat_list guild_evolutions
 do
 	mysqldump -u<user> -p<password> --opt guionbotdb $table > /home/pi/GuionBot/SQLBACKUP/$table.sql
 done
+cp -r /home/pi/GuionBot/EVENTS /home/pi/GuionBot/SQLBACKUP
+
 tar -zcvf /home/pi/GuionBot/all_tables.gz /home/pi/GuionBot/SQLBACKUP
 mv /home/pi/GuionBot/all_tables.gz /home/pi/GuionBot/SQLBACKUP
 
-python /home/pi/GuionBot/guionbot-master/upload_sqlbackup.py $1
+python /home/pi/GuionBot/guionbot-master/SCRIPTS/upload_sqlbackup.py $1
 
