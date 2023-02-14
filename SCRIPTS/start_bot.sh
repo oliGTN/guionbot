8301eca44d7d9151c3253055f1592ed91352203e
oliGTN <olivier.w.guitton@gmail.com>
2023-02-14 18:30:01 +0000

    typo

--- SCRIPTS/start_bot.sh
+++ SCRIPTS/start_bot.sh
@@ -5,16 +5,16 @@ cd /home/pi/GuionBot/swgoh-stat-calc
 node server.js &
 
 # GUIONBOT
-if [ "$1" = "" ] then
+if [ "$1" = "" ]; then
 	cd /home/pi/GuionBot/guionbot-master
 else
 	cd $CMD_PWD
 fi
 
 rm CACHE/*.tmp 2>/dev/null
-if [ "$1" = "test" ] then
+if [ "$1" = "test" ]; then
 	python guionbot_discord.py test
-elif [ "$1" = "noloop" ] then
+elif [ "$1" = "noloop" ]; then
 	python guionbot_discord.py noloop
 else
 	python guionbot_discord.py | while read line; do echo "$line" >> /home/pi/GuionBot/LOGS/guionbot.log; done
