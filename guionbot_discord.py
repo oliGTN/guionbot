ef7acee29bbd8f37572d00c779ae1bd3d0331ab0
oliGTN <olivier.w.guitton@gmail.com>
2023-02-22 20:33:32 +0100

    allow cache data for get_rpc_data

--- guionbot_discord.py
+++ guionbot_discord.py
@@ -228,7 +228,7 @@ async def bot_loop_60minutes():
 
         try:
             for guild in bot.guilds:
-                await bot.loop.run_in_executor(None, connect_rpc.get_rpc_data, guild.name)
+                await bot.loop.run_in_executor(None, connect_rpc.get_rpc_data, guild.name, False)
 
         except Exception as e:
             goutils.log("ERR", "guionbot_discord.bot_loop_60minutes", str(sys.exc_info()[0]))
