3d37ba2bd2d81729674a342de88e3322ad0dbf23
oliGTN <olivier.w.guitton@gmail.com>
2023-03-06 21:58:51 +0100

    create DATA dict for llocalization from RPC

---
+++ SCRIPTS/update_localization_dict.py
@@ -0,0 +1,21 @@
+import json
+import os
+
+localization_path = "../warstats/LocalizationBundle"
+for filename in os.listdir(localization_path):
+    lang = filename[4:10]
+    dict_lang = {}
+    f = open(localization_path+"/"+filename, 'r')
+    for line in f.readlines():
+        if line[0] == "#":
+            continue
+        tab_line = line.split('|')
+        key = tab_line[0]
+        value = tab_line[1]
+        dict_lang[key]=value
+    f.close()
+    jsonname = "../DATA/"+lang+".json"
+    f = open(jsonname, 'w')
+    f.write(json.dumps(dict_lang, sort_keys=True, indent=4))
+    f.close()
+    print(jsonname+" succesfully written")
