baf461dfc2e825a80f5c82460fd1ba7690ce6a71
oliGTN <olivier.w.guitton@gmail.com>
2023-03-26 22:26:56 +0200

    auto detect data file

--- SCRIPTS/update_local_data.sh
+++ SCRIPTS/update_local_data.sh
@@ -1,2 +1,7 @@
-python SCRIPTS/update_unitsList_json.py
-python SCRIPTS/update_units_zetas_omicrons.py
+python SCRIPTS/update_localization_dict.py
+
+GAMEDATA_VERSION = $(grep latestGamedataVersion ../warstats/metadata.json |cut -f4 -d")
+echo $GAMEDATA_VERSION
+GAMEDATA_FILE = ../warstats/GameData_${GAMEDATA_VERSION}.json
+python SCRIPTS/update_unitsList_json.py $GAMEDATA_FILE
+python SCRIPTS/update_units_zetas_omicrons.py $GAMEDATA_FILE
