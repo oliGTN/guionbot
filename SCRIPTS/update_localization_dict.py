e17c61ce311be5f22bba15f026bc9b6e2d814210
oliGTN <olivier.w.guitton@gmail.com>
2023-04-03 22:15:36 +0200

    use keymap from localization

--- SCRIPTS/update_localization_dict.py
+++ SCRIPTS/update_localization_dict.py
@@ -2,6 +2,18 @@ import json
 import os
 
 localization_path = "../warstats/LocalizationBundle"
+key_map = localization_path+"/Loc_Key_Mapping.txt"
+dict_map = {}
+f = open(key_map)
+for line in f.readlines():
+    if line[0] == "#":
+        continue
+    tab_line = line.split('|')
+    key = tab_line[0].strip()
+    value = tab_line[1].strip()[2:]
+    dict_map[value]=key
+f.close()
+
 for filename in os.listdir(localization_path):
     lang = filename[4:10]
     if lang in ["FRE_FR", "ENG_US"]:
@@ -14,9 +26,16 @@ for filename in os.listdir(localization_path):
             key = tab_line[0].strip()
             value = tab_line[1].strip()
             dict_lang[key]=value
+
+            if key in dict_map:
+                dict_lang[dict_map[key]]=value
+
         f.close()
         jsonname = "../DATA/"+lang+".json"
         f = open(jsonname, 'w')
         f.write(json.dumps(dict_lang, sort_keys=True, indent=4))
         f.close()
         print(jsonname+" succesfully written")
+
+key_map = localization_path+"/Loc_Key_Mapping.txt"
+
