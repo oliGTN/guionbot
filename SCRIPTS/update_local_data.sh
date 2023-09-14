337a24cb5bd97a00d75265336bd9195db8f0b47a
oliGTN <olivier.w.guitton@gmail.com>
2023-09-14 20:46:31 +0200

    remove CACHE file at the end

--- SCRIPTS/update_local_data.sh
+++ SCRIPTS/update_local_data.sh
@@ -5,3 +5,5 @@ echo $GAMEDATA_VERSION
 GAMEDATA_FILE=../warstats/GameData_${GAMEDATA_VERSION}.json
 python SCRIPTS/update_unitsList_json.py $GAMEDATA_FILE
 python SCRIPTS/create_capa_list.py $GAMEDATA_FILE
+
+rm CACHE/config_units.json
