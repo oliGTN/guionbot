eb7f472567d0acf4d8be68e092c9c1de26433bb7
oliGTN <olivier.w.guitton@gmail.com>
2023-10-27 15:16:44 +0200

    fix copy of events

--- SCRIPTS/mysql_backup.sample.sh
+++ SCRIPTS/mysql_backup.sample.sh
@@ -2,12 +2,24 @@ rm -rf /home/pi/GuionBot/SQLBACKUP/
 mkdir /home/pi/GuionBot/SQLBACKUP
 for table in gp_history gv_history roster_evolutions guilds guild_bot_infos players shards stat_list guild_evolutions
 do
+    echo $table
 	mysqldump -u<user> -p<password> --opt guionbotdb $table > /home/pi/GuionBot/SQLBACKUP/$table.sql
 done
-cp -r /home/pi/GuionBot/EVENTS /home/pi/GuionBot/SQLBACKUP
 
+echo Copy EVENTS... only latest 30 days
+OIFS="$IFS"
+IFS=$'\n'
+for f in $(find /home/pi/GuionBot/EVENTS/ -type f -mtime -30)
+do
+    echo "   $f"
+    cp $f /home/pi/GuionBot/SQLBACKUP
+done
+IFS="$OIFS"
+
+echo Compress...
 tar -zcvf /home/pi/GuionBot/all_tables.gz /home/pi/GuionBot/SQLBACKUP
 mv /home/pi/GuionBot/all_tables.gz /home/pi/GuionBot/SQLBACKUP
 
-python /home/pi/GuionBot/guionbot-master/SCRIPTS/upload_sqlbackup.py $1
+echo Upload...
+python /home/pi/GuionBot/guionbot-dev/SCRIPTS/upload_sqlbackup.py $1
 
