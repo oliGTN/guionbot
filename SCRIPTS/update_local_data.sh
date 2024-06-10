fae5bf62d8c9a6dbfa44f9bfaec3e85676d08505
oliGTN <olivier.w.guitton@gmail.com>
2024-06-11 01:24:22 +0200

    compute eqpt_dict

--- SCRIPTS/update_local_data.sh
+++ SCRIPTS/update_local_data.sh
@@ -7,6 +7,8 @@ echo "update units..."
 python SCRIPTS/update_unitsList_json.py $GAMEDATA_FILE
 echo "update capabilities..."
 python SCRIPTS/create_capa_list.py $GAMEDATA_FILE
+echo "update equipment..."
+python SCRIPTS/create_eqpt_dict.py $GAMEDATA_FILE
 echo "update TB..."
 python SCRIPTS/create_tb_data.py $GAMEDATA_FILE
 
