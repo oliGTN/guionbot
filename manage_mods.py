2abc95164a806afad672f478e4e38792ae66ced0
oliGTN <olivier.w.guitton@gmail.com>
2024-09-29 02:22:04 +0200

    typo

--- manage_mods.py
+++ manage_mods.py
@@ -478,11 +478,6 @@ async def apply_mod_allocations(mod_allocations, allyCode, is_simu, interaction,
                 dict_player_mods[allocated_mod_id]["unit_id"] = target_char_defId
                 dict_player["rosterUnit"][target_char_defId]["equippedStatMod"][mod_slot] = allocated_mod_id
 
-            #Manage display to user
-            if (time.time() - prev_display_time) > 10:
-                await interaction.edit_original_response(content="Reste "+str(len(mod_allocations))+"/"+str(original_unit_count)+"...")
-                prev_display_time = time.time()
-
         if len(mods_to_add) > 0:
             #write update request
             mods_txt = ""
@@ -521,6 +516,11 @@ async def apply_mod_allocations(mod_allocations, allyCode, is_simu, interaction,
 
         #remove done allocation "a" from the list
         mod_allocations.remove(a)
+
+        #Manage display to user
+        if (time.time() - prev_display_time) > 10:
+            await interaction.edit_original_response(content="Reste "+str(len(mod_allocations))+"/"+str(original_unit_count)+"...")
+            prev_display_time = time.time()
 
     goutils.log2("INFO", "Max inventory: "+str(max_inventory))
     needed_inventory = max_inventory-initial_inventory
