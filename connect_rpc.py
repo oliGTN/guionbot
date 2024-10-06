c333de59f013976d5643032401b1c4bacc77606a
oliGTN <olivier.w.guitton@gmail.com>
2024-10-06 19:31:08 +0200

    typo

--- connect_rpc.py
+++ connect_rpc.py
@@ -46,8 +46,9 @@ async def release_sem(id):
 def get_dict_bot_accounts():
     query = "SELECT guild_id, bot_allyCode, bot_locked_until, priority_cache, " \
             "twChanOut_id, tbChanOut_id, tbChanEnd_id, name " \
-            "FROM guild_bot_infos where bot_allyCode != '' " \
+            "FROM guild_bot_infos " \
             "JOIN guilds ON guilds.id=guild_bot_infos.guild_id " 
+            "WHERE bot_allyCode != '' "
     #goutils.log2("DBG", query)
     db_data = connect_mysql.get_table(query)
 
