5104373ce763c1127e2f8e7b5c0e883ca5dce3db
oliGTN <olivier.w.guitton@gmail.com>
2025-06-01 16:53:57 +0200

    cleanup admin dashboard

--- html/dashboard.php
+++ html/dashboard.php
@@ -40,7 +40,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['deassociate_guilds'])
 
     foreach ($guild_ids as $guild_id) {
         $query = "DELETE FROM user_guilds WHERE user_id = :user_id AND guild_id = :guild_id";
-        error_log($query.' '.$user_id.' '.$guild_id);
+        //error_log($query.' '.$user_id.' '.$guild_id);
         $stmt = $conn->prepare($query);
         $stmt->execute(['user_id' => $user_id, 'guild_id' => $guild_id]);
     }
@@ -51,11 +51,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['deassociate_guilds'])
 if ($isAdmin) {
     try {
         // Prepare the SQL query to fetch the specific guild associations from the DB table
-        $query = "SELECT users.user_id AS user_id, users.name AS name, GROUP_CONCAT(CONCAT(' ',guilds.name)) AS guild_names FROM user_guilds";
-        $query .= " JOIN users ON users.user_id=user_guilds.user_id";
-        $query .= " JOIN guionbotdb.guilds ON guionbotdb.guilds.id=guild_id";
+        $query = "SELECT users.user_id AS user_id, users.name AS name, GROUP_CONCAT(CONCAT(' ',guilds.name)) AS guild_names FROM users";
+        $query .= " LEFT JOIN user_guilds ON users.user_id=user_guilds.user_id";
+        $query .= " LEFT JOIN guionbotdb.guilds ON guionbotdb.guilds.id=guild_id";
         $query .= " GROUP BY user_id ORDER BY users.name";
-        #error_log($query);
+        //error_log($query);
         $stmt = $conn->prepare($query);
         $stmt->execute();
 
@@ -65,7 +65,7 @@ if ($isAdmin) {
     } catch (PDOException $e) {
         echo "Error fetching users: " . $e->getMessage();
     }
-} else {
+} 
     try {
         // Prepare the SQL query to fetch the user infos
         $query = "SELECT id, name FROM guilds";
@@ -97,7 +97,6 @@ if ($isAdmin) {
         echo "Error fetching my_bonus_guilds: " . $e->getMessage();
     }
     
-}
 try {
     // Prepare the SQL query to fetch all guilds from the DB table
     $stmt = $conn_guionbot->prepare("SELECT id, name FROM guilds ORDER BY name");
@@ -142,50 +141,58 @@ try {
                 <p style="color:green;display:inline">You are logged in as an administrator</p>
             </div>
 
-        <!-- Table to display user names -->
-        <h2>List of Specific user guild associations</h2>
-        <table>
-            <tr>
-                <th>user</th>
-                <th>associated guilds</th>
-            </tr>
-            <?php
-            // Loop through each player and display in a table row
-            if (!empty($user_guilds)) {
-                foreach ($user_guilds as $user) {
-                    echo "<tr><td>" . $user['name'] . " (".$user['user_id'].")</td><td>" . htmlspecialchars($user['guild_names']) . "</td></tr>";
-                }
-            } else {
-                echo "<tr><td colspan='2'>No association found.</td></tr>";
-            }
-            ?>
-        </table>
+            <div class="card">
+                <!-- Table to display user names -->
+                <h3>Admin panel</h3>
+                <div class="card">
+                    <table>
+                        <tr>
+                            <th>user</th>
+                            <th>associated guilds</th>
+                        </tr>
+                        <?php
+                        // Loop through each player and display in a table row
+                        if (!empty($user_guilds)) {
+                            foreach ($user_guilds as $user) {
+                                if ($user['guild_names'] != '') {
+                                    echo "<tr><td>" . $user['name'] . " (".$user['user_id'].")</td><td>" . htmlspecialchars($user['guild_names']) . "</td></tr>";
+                                }
+                            }
+                        } else {
+                            echo "<tr><td colspan='2'>No association found.</td></tr>";
+                        }
+                        ?>
+                    </table>
+                </div>
 
-        <h3>Associate Guilds with Users</h3>
-        <form method="POST" action="">
-            <label for="user_id">Select User:</label><br>
-            <select name="user_id" id="user_id" required>
-                <?php foreach ($user_guilds as $user): ?>
-                    <option value="<?php echo $user['id']; ?>"><?php echo htmlspecialchars($user['email']); ?></option>
-                <?php endforeach; ?>
-            </select><br>
+                <div class="card">
+                    <form method="POST" action="">
+                        <label for="user_id">Select User:</label><br>
+                        <select name="user_id" id="user_id" required>
+                            <?php foreach ($user_guilds as $user): ?>
+                                <option value="<?php echo $user['user_id']; ?>"><?php echo htmlspecialchars($user['name']); ?></option>
+                            <?php endforeach; ?>
+                        </select><br>
 
-            <label for="guild_ids">Select Guild(s):</label><br>
-            <select name="guild_ids[]" id="guild_ids" multiple required>
-                <?php foreach ($guilds as $guild): ?>
-                    <option value="<?php echo $guild['id']; ?>"><?php echo htmlspecialchars($guild['name']); ?></option>
-                <?php endforeach; ?>
-            </select><br>
+                        <label for="guild_ids">Select Guild(s):</label><br>
+                        <select name="guild_ids[]" id="guild_ids" multiple required>
+                            <?php foreach ($guilds as $guild): ?>
+                                <option value="<?php echo $guild['id']; ?>"><?php echo htmlspecialchars($guild['name']); ?></option>
+                            <?php endforeach; ?>
+                        </select><br>
 
-            <button type="submit" name="associate_guilds">Associate Guilds</button>
-            <button type="submit" name="deassociate_guilds">De-associate Guilds</button>
-        </form>
-    
+                        <button type="submit" name="associate_guilds">Associate Guilds</button>
+                        <button type="submit" name="deassociate_guilds">De-associate Guilds</button>
+                    </form>
+                </div>
+            </div>
+        
         <?php else: ?>
             <div class="card">
                 <p style="color:green;display:inline">You are logged in as a user: <?php echo $_SESSION['user_name'] ?></p>
             </div>
 
+        <?php endif; ?>
             <div class="card">
             <table>
                 <tr>
@@ -219,7 +226,6 @@ try {
             </table>
             </div>
             <?php endif; ?> <!-- !empty($my_bonus_guilds) -->
-        <?php endif; ?>
     </div> <!-- container -->
     </div> <!-- site-content -->
     <div class="site-cache" id="site-cache" onclick="document.body.classList.toggle('with--sidebar')"></div>
