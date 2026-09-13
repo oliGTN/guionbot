9adce98d45dab32bfefcafe774b3a22ca77852c8
oliGTN <71192804+oliGTN@users.noreply.github.com>
2026-09-13 19:47:38 +0200

    Load Material Icons on guild page

--- html/g.php
+++ html/g.php
@@ -36,6 +36,7 @@ try {
     <title>GuiOn bot for SWGOH</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="basic.css"><link rel="stylesheet" href="tables.css"><link rel="stylesheet" href="navbar.css"><link rel="stylesheet" href="main.1.008.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
 <body>
 <div class="site-container"><div class="site-pusher">
