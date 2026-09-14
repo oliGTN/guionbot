1a95f5a8adbbcc7765fe162e87a9b929835028b5
oliGTN <71192804+oliGTN@users.noreply.github.com>
2026-09-14 13:58:55 +0200

    Prettify pvariables.php without changing security logic

--- html/pvariables.php
+++ html/pvariables.php
@@ -1,12 +1,19 @@
 <?php
-function set_session_rights_for_allycode($allycode) {
+function set_session_rights_for_allycode($allycode)
+{
     if (!isset($_SESSION['user_id'])) {
         return [false, false];
     }
+
     $allyCodes = $_SESSION['allyCodes'] ?? [];
-    $key = (int)$allycode;
-    $isMyAllycode = array_key_exists($key, $allyCodes) || array_key_exists($allycode, $allyCodes);
-    $confirmed = array_key_exists($key, $allyCodes) ? $allyCodes[$key] : ($allyCodes[$allycode] ?? false);
+    $key = (int) $allycode;
+    $isMyAllycode = array_key_exists($key, $allyCodes)
+        || array_key_exists($allycode, $allyCodes);
+
+    $confirmed = array_key_exists($key, $allyCodes)
+        ? $allyCodes[$key]
+        : ($allyCodes[$allycode] ?? false);
+
     return [$isMyAllycode, $isMyAllycode && !empty($confirmed)];
 }
 ?>
