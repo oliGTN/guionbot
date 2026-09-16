54a223d8fe9f4ea35034d103f77b68dff960efba
oliGTN <71192804+oliGTN@users.noreply.github.com>
2026-09-17 00:36:43 +0200

    Restructure player page like guild page

--- html/p.php
+++ html/p.php
@@ -66,31 +66,7 @@ try {
 
         <div class="site-content">
             <div class="container">
-                <h2>
-                    <?php echo h($player['name']); ?>
-                    <a href="https://swgoh.gg/p/<?php echo rawurlencode($allycode); ?>">
-                        <img
-                            src="IMAGES/LOGOS/swgohgg_logo.png"
-                            width="50"
-                            alt="swgoh.gg"
-                        />
-                    </a>
-                </h2>
-
-                <div class="card">
-                    <p style="color:green;display:inline">
-                        <?php echo $isMyAllycode ? 'This is your account' : ''; ?>
-                    </p>
-                    <p style="color:red;display:inline">
-                        <br/>
-                        <?php echo $isAdmin ? 'You are logged as an administrator' : ''; ?>
-                    </p>
-                </div>
-
-                <h3>Player current guild</h3>
-                <a href="g.php?gid=<?php echo rawurlencode((string) $player['guild_id']); ?>">
-                    <?php echo h($player['guild_name']); ?>
-                </a>
+                <?php include 'pheader.php'; ?>
 
                 <h3>Player past guilds</h3>
                 <div class="card">
