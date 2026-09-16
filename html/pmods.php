94ec30aadd52d59f57baf2eb7e49ef1f09e519f6
oliGTN <71192804+oliGTN@users.noreply.github.com>
2026-09-17 00:36:55 +0200

    Use shared player header on mods page

--- html/pmods.php
+++ html/pmods.php
@@ -44,32 +44,7 @@ include 'pdata.php';
 
         <div class="site-content">
             <div class="container">
-                <h2>
-                    <?php echo h($player['name']); ?>
-                    <a href="https://swgoh.gg/p/<?php echo rawurlencode($allycode); ?>">
-                        <img src="IMAGES/LOGOS/swgohgg_logo.png" width="50" alt="swgoh.gg" />
-                    </a>
-                </h2>
-
-                <div class="card">
-                    <p style="color:green;display:inline">
-                        <?php echo $isMyAllycode ? 'This is your account' : ''; ?>
-                    </p>
-                    <p style="color:red;display:inline">
-                        <br/>
-                        <?php echo $isAdmin ? 'You are logged as an administrator' : ''; ?>
-                    </p>
-                </div>
-
-                <h3>Player current guild</h3>
-                <a href="g.php?gid=<?php echo rawurlencode((string) $player['guild_id']); ?>">
-                    <?php echo h($player['guild_name']); ?>
-                </a>
-
-                <h3>Player allyCode</h3>
-                <p><?php echo h($allycode); ?></p>
-
-                <?php include 'pnavbar.php'; ?>
+                <?php include 'pheader.php'; ?>
 
                 <h3>Player Mods</h3>
             </div>
