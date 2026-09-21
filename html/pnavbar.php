66492f99a65388aa5f257a43f52e5950970cc96e
oliGTN <71192804+oliGTN@users.noreply.github.com>
2026-09-21 23:10:20 +0200

    Add My TW player navigation option

--- html/pnavbar.php
+++ html/pnavbar.php
@@ -2,4 +2,5 @@
 <div class="navbar">
     <a href="/p.php?ac=<?php echo rawurlencode((string) $allycode); ?>" class="<?php echo substr($_SERVER['REQUEST_URI'], 0, strlen('/p.php')) == '/p.php' ? 'active' : ''; ?>">Guild history</a>
     <a href="/pmods.php?ac=<?php echo rawurlencode((string) $allycode); ?>" class="<?php echo substr($_SERVER['REQUEST_URI'], 0, strlen('/pmods.php')) == '/pmods.php' ? 'active' : ''; ?>">Mods</a>
+    <a href="/ptw.php?ac=<?php echo rawurlencode((string) $allycode); ?>" class="<?php echo substr($_SERVER['REQUEST_URI'], 0, strlen('/ptw.php')) == '/ptw.php' ? 'active' : ''; ?>">My TW</a>
 </div>
