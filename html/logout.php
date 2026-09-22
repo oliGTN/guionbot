f666eab0ed5dabab4950294cf65ee350ee02b828
oliGTN <71192804+oliGTN@users.noreply.github.com>
2026-09-23 01:30:52 +0200

    Clear Discord auth cookie on logout

--- html/logout.php
+++ html/logout.php
@@ -1,6 +1,15 @@
 <?php
 session_start();
 session_destroy();
+
+$cookie_options = [
+    'expires' => time() - 3600,
+    'path' => '/',
+    'secure' => !empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off',
+    'httponly' => true,
+    'samesite' => 'Lax',
+];
+setcookie('discord_access_token', '', $cookie_options);
 header("Location: index.php");
 exit();
 ?>
