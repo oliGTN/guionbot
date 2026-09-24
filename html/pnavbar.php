f152929546b17cd8755b2ca4bd1561162666109d
oliGTN <71192804+oliGTN@users.noreply.github.com>
2026-09-25 00:41:40 +0200

    Add Datacrons to player navigation

--- html/pnavbar.php
+++ html/pnavbar.php
@@ -2,5 +2,6 @@
 <div class="navbar">
     <a href="/p.php?ac=<?php echo rawurlencode((string) $allycode); ?>" class="<?php echo substr($_SERVER['REQUEST_URI'], 0, strlen('/p.php')) == '/p.php' ? 'active' : ''; ?>">Guild history</a>
     <a href="/pmods.php?ac=<?php echo rawurlencode((string) $allycode); ?>" class="<?php echo substr($_SERVER['REQUEST_URI'], 0, strlen('/pmods.php')) == '/pmods.php' ? 'active' : ''; ?>">Mods</a>
+    <a href="/pdatacrons.php?ac=<?php echo rawurlencode((string) $allycode); ?>" class="<?php echo substr($_SERVER['REQUEST_URI'], 0, strlen('/pdatacrons.php')) == '/pdatacrons.php' ? 'active' : ''; ?>">Datacrons</a>
     <a href="/ptw.php?ac=<?php echo rawurlencode((string) $allycode); ?>" class="<?php echo substr($_SERVER['REQUEST_URI'], 0, strlen('/ptw.php')) == '/ptw.php' ? 'active' : ''; ?>">TW</a>
 </div>
